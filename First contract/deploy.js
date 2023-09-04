1102cfe3ebee131984aa8d31d12507a86c99786c
unknown <pingo15102002@gmail.com>
2023-09-04 19:16:12 +0200

    Finished contract developement

--- First contract/deploy.js
+++ First contract/deploy.js
@@ -1,5 +1,28 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const { interface, bytecode } = require('./compile');
+require('dotenv').config();
 
 // deploy code will go here
+
+const provider = new HDWalletProvider(
+	process.env.NEUMONIC,
+	'https://sepolia.infura.io/v3/a84f1ad53c5844aab609709f988f7fe2'
+);
+
+const web3 = new Web3(provider);
+
+const deployContract = async () => {
+	const accounts = await web3.eth.getAccounts();
+
+	const deployResult = await new web3.eth.Contract(JSON.parse(interface))
+		.deploy({
+			data: bytecode,
+			arguments: ['Hi there!'],
+		})
+		.send({ gas: '1000000', from: accounts[0] });
+
+	console.log('Contract deployed to: ', deployResult.options.address);
+};
+
+deployContract();
--- First contract/test/Inbox.test.js
+++ First contract/test/Inbox.test.js
@@ -5,6 +5,8 @@ const { Web3 } = require('web3');
 // Import Inbox smart contract
 const { interface, bytecode } = require('../compile');
 
+const INITIAL_STRING = 'Hi there!';
+
 // contract test code will go here
 
 const web3 = new Web3(ganache.provider());
@@ -18,13 +20,31 @@ beforeEach(async () => {
 
 	// Use one of those accounts to deploy the contract
 	inbox = await new web3.eth.Contract(JSON.parse(interface))
-		.deploy({ data: bytecode, arguments: ['Hi there!'] })
+		.deploy({ data: bytecode, arguments: [INITIAL_STRING] })
 		.send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox contract', () => {
+	// Verify smart contract deployment
 	it('deploys a contract', () => {
 		console.log(inbox);
+		// check if inbox contract has address atached to it (that means that deployment was succesfull)
+		assert.ok(inbox.options.address);
+	});
+
+	// Test	initial message of smart contract
+	it('has a defeault message', async () => {
+		const message = await inbox.methods.message().call();
+
+		assert.equal(message, INITIAL_STRING);
+	});
+
+	// Test for set message function of smart contract
+	it('can change message', async () => {
+		await inbox.methods.setMessage('Bye').send({ from: accounts[0] });
+
+		const message = await inbox.methods.message().call();
+		assert.equal(message, "bye")
 	});
 });
 
